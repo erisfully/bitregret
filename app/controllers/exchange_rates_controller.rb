7a8720b86617899f60d0038a93fa1d495a627806
Ashley <a.clarkem@gmail.com>
2014-11-05 15:54:45 -0700

    working!

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -3,6 +3,9 @@ class ExchangeRatesController < ApplicationController
   def index
     @date = ExchangeRate.return_date(params[:start_date][:year], params[:start_date][:month], params[:start_date][:day])
     @all = ExchangeRate.all
+    @amount = ExchangeRate.return_amount(params[:start_date][:year], params[:start_date][:month], params[:start_date][:day], params[:amount])
+    @worth_today = ExchangeRate.worth_today(params[:start_date][:year], params[:start_date][:month], params[:start_date][:day], params[:amount])
+
   end
 
 end
--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -14,7 +14,20 @@ class ExchangeRate < Connect
   end
 
   def self.return_date(year, month, day)
-    all.select{|h|(unix_to_utc(h["x"])) == Date.new(year.to_i,month.to_i,day.to_i)}
+    all.select { |h| (unix_to_utc(h["x"])) == Date.new(year.to_i, month.to_i, day.to_i) }
+  end
+
+  def self.return_amount(year, month, day, amount)
+    one_bitcoin_in_dollars = return_date(year, month, day).first["y"]
+    number_of_bitcoins = amount.to_i/one_bitcoin_in_dollars
+    number_of_bitcoins.round(2)
+  end
+
+  def self.worth_today(year, month, day, amount)
+    todays_coordinates = all.select { |h| (unix_to_utc(h["x"])) == Date.today }
+    todays_worth = todays_coordinates.first["y"]
+    number_of_bitcoins = return_amount(year, month, day, amount)
+    todays_worth * number_of_bitcoins
   end
 
   private
@@ -27,10 +40,6 @@ class ExchangeRate < Connect
 end
 
 
-#convert date to unix timestamp **** match just first five numbers? Figure out how to deal with the fact that
-  # it doesn't match up perfectly because of seconds. maybe turn the x into utc first***
-  #** use select to find the matching dates***
-#search array for hash with key that matches that timestamp
 #return y value for that hash
 #divide amount entered by y value returned
 
